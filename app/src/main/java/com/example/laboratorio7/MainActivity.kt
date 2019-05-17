112b384da75f856ae3d48e6a67086943cdbab8f8
marielosagui <00078913@uca.edu.sv>
2019-05-17 15:43:56 -0600

    no vimos el resultado xD

--- app/src/main/java/com/example/laboratorio7/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio7/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.laboratorio7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.laboratorio7.adapters.ReposAdapter
+import com.example.laboratorio7.database.entities.GithubRepo
+import com.example.laboratorio7.database.viewmodels.GithubRepoViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  adapter: ReposAdapter
+    lateinit var viewModel: GithubRepoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun bind(){
+        adapter= ReposAdapter(ArrayList())
+        viewModel=ViewModelProviders.of(this).get(GithubRepoViewModel::class.java)
+        rv_repos.apply {
+            adapter=this@MainActivity.adapter
+            layoutManager =LinearLayoutManager(this@MainActivity)
+        }
+        viewModel.getAll().observe(this, Observer {
+            adapter.updateList(it)
+        })
+        bt_add.setOnClickListener {
+            viewModel.insert(GithubRepo(et_repo_name.text.toString()))
+        }
     }
 }
